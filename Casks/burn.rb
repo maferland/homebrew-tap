2928aa012e644121e25dba03e189a80b6e040f9a
Anka <runner@sat12-dp149-42469d56-1575-4e2c-bdee-d992216b5886-5A3B373B96F1.local>
2026-02-25 19:17:44 +0000

    Update burn to v1.4.1

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,6 +1,6 @@
 cask "burn" do
-  version "1.4.0"
-  sha256 "c50d68396a4208c199236fe6e5a2a2b136bc3f57b692f08241901e25fb67d6e2"
+  version "1.4.1"
+  sha256 "f235db82a17e7b11b11406b30178d149d39139b22e0ef0c5fbcd41558ef37b4d"
 
   url "https://github.com/maferland/burn/releases/download/v#{version}/Burn-v#{version}-macos.dmg"
   name "Burn"
