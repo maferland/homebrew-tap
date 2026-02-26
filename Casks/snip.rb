c06f13128e26635329705d5b48457de042ffacd3
Anka <runner@iad20-gt1020-af940f55-c8f7-4c49-86f9-88de0ba36cb7-B6CB574D2457.local>
2026-02-26 00:48:41 +0000

    Update snip to v2.4.0

--- Casks/snip.rb
+++ Casks/snip.rb
@@ -1,11 +1,13 @@
 cask "snip" do
-  version "2.2.0"
-  sha256 "ef46d1ac95ba15cb4fd4ca551255aa9c114ba55be5c1ac90e1b5262d0cd797d5"
+  version "2.4.0"
+  sha256 "235f056a04cba12eaf6a9a8997be7208d67d6481047a5908b58a2cf862d59689"
 
   url "https://github.com/maferland/snip/releases/download/v#{version}/Snip-v#{version}-macos.dmg"
   name "Snip"
   desc "Automatically strip tracking parameters from copied URLs"
   homepage "https://github.com/maferland/snip"
+
+  depends_on macos: ">= :sonoma"
 
   livecheck do
     url :url
