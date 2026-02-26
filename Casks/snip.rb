88107c3ed682455f7a0f98f3b759bb4bff7850cc
Anka <runner@sat12-bq159-79ab5df2-5f32-419e-b4c6-26b50fb260ec-E22FABE530A7.local>
2026-02-26 01:52:29 +0000

    Update snip to v2.6.0

--- Casks/snip.rb
+++ Casks/snip.rb
@@ -1,6 +1,6 @@
 cask "snip" do
-  version "2.5.0"
-  sha256 "fbe9d7767e91a3045898ef6336efd4df55345db1162926ea23b4300fec481504"
+  version "2.6.0"
+  sha256 "fc1b01439c8e3ef1560d48d21b2931fcad6476336bdfb7254c0ffe109354bd4c"
 
   url "https://github.com/maferland/snip/releases/download/v#{version}/Snip-v#{version}-macos.dmg"
   name "Snip"
