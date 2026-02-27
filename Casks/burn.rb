a1f927bf64b7547ebb50d868486106da27be4dea
Marc-Antoine <marc-antoine.ferland@carta.com>
2026-02-26 20:20:11 -0500

    Update burn to v1.8.0

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,6 +1,6 @@
 cask "burn" do
-  version "1.7.0"
-  sha256 "db0a909d8f5b3f7cd4892bb1af93aa521834ff354fbf3b811bd2b302303202a9"
+  version "1.8.0"
+  sha256 "9aff04d17cb85e858e6eda54b206afd59608a574bcf2490d6ccf013c4fb10237"
 
   url "https://github.com/maferland/burn/releases/download/v#{version}/Burn-v#{version}-macos.dmg"
   name "Burn"
