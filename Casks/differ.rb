5a6dbf57507a27a797bc0a6160d02478c0d5ada4
Marc-Antoine <marc-antoine.ferland@carta.com>
2026-03-10 07:37:17 -0400

    Update differ cask URLs after repo rename

--- Casks/differ.rb
+++ Casks/differ.rb
@@ -2,10 +2,10 @@ cask "differ" do
   version "0.1.0"
   sha256 "e32281037763edec4e242eaa17c0ccba613964c485bfe89b1987b8f0e9c42f89"
 
-  url "https://github.com/maferland/diff/releases/download/v#{version}/Differ_#{version}_universal.dmg"
+  url "https://github.com/maferland/differ/releases/download/v#{version}/Differ_#{version}_universal.dmg"
   name "Differ"
   desc "Desktop image comparison tool with pixel-level diffing"
-  homepage "https://github.com/maferland/diff"
+  homepage "https://github.com/maferland/differ"
 
   depends_on macos: ">= :high_sierra"
 
