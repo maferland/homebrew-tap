a7d586dffe25306dfc3dd3ee852f3a029c0420bc
Marc-Antoine <marc-antoine.ferland@carta.com>
2026-03-10 07:23:35 -0400

    Add differ cask v0.1.0

---
+++ Casks/differ.rb
@@ -0,0 +1,20 @@
+cask "differ" do
+  version "0.1.0"
+  sha256 "e32281037763edec4e242eaa17c0ccba613964c485bfe89b1987b8f0e9c42f89"
+
+  url "https://github.com/maferland/diff/releases/download/v#{version}/Differ_#{version}_universal.dmg"
+  name "Differ"
+  desc "Desktop image comparison tool with pixel-level diffing"
+  homepage "https://github.com/maferland/diff"
+
+  depends_on macos: ">= :high_sierra"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Differ.app"
+
+  zap trash: "~/Library/Preferences/com.maferland.differ.plist"
+end
