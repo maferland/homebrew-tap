575e2b111edd577ceaafd52d16bc98e1f94afde4
Marc-Antoine <marc-antoine.ferland@carta.com>
2026-03-12 05:20:35 -0400

    Update tidy to v1.2.0

--- Casks/tidy.rb
+++ Casks/tidy.rb
@@ -1,6 +1,6 @@
 cask "tidy" do
-  version "1.1.1"
-  sha256 "96f11f5c18773c0725c613c584bb4207551d6111cee07310827797918ad123c9"
+  version "1.2.0"
+  sha256 "55af37d2f32aaa8147ff426e9181089f299e660caf0715a7ec030a6173511967"
 
   url "https://github.com/maferland/tidy/releases/download/v#{version}/Tidy-v#{version}-macos.dmg"
   name "Tidy"
