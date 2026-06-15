34e44dcd93725d3afb5965dfc0a041acc8b5b2b7
Marc-Antoine <marc-antoine.ferland@carta.com>
2026-06-15 18:34:41 -0400

    Update burn to v1.12.1

--- Casks/burn.rb
+++ Casks/burn.rb
@@ -1,13 +1,13 @@
 cask "burn" do
-  version "1.12.0"
-  sha256 "81a87af44010efc12845efe4d51e039238fda2beee4b9293a90e4ffa2ddbd75b"
+  version "1.12.1"
+  sha256 "cb16b7b83787d46e802076506902ce024ad81cc3bb65dcf41d382040bad0093a"
 
   url "https://github.com/maferland/burn/releases/download/v#{version}/Burn-v#{version}-macos.dmg"
   name "Burn"
   desc "Track Claude Code spending from the macOS menu bar"
   homepage "https://github.com/maferland/burn"
 
-  depends_on macos: :sonoma
+  depends_on macos: ">= :sonoma"
 
   livecheck do
     url :url
